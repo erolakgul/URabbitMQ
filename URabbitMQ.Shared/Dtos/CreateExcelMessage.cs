05f8208697ff1bd212d897117abaf0f37b78d6a9
erol.akgul <erolakgul88@gmail.com>
2024-05-10 08:45:28 +0300

    excel kaydetme aşamaında mesajın rabbit mq ya iletilmesi için gerekli publisher class ı oluşturuldu

---
+++ URabbitMQ.Shared/Dtos/CreateExcelMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace URabbitMQ.Shared.Dtos
+{
+    public class CreateExcelMessage
+    {
+        public string? UserID { get; set; }
+        public int FileId { get; set; }
+        // worker service ile db den çekeceğiz verileri, taşıma olmayacak
+        //public List<Product>? Products { get; set; }
+    }
+}
