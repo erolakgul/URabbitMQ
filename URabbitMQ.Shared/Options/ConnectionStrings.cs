3f5645d46547260f8fd838c0caea637550cc4c52
erol.akgul <erolakgul88@gmail.com>
2024-05-14 14:21:20 +0300

    web2 appsettings teki verileri option pattern ile worker servicinde kullandık,
    file controller ve product controller ile çalışıldı,
    
    publisher da  tabbit service ine bağlanma işlemi yapıcı methoda alındı,

---
+++ URabbitMQ.Shared/Options/ConnectionStrings.cs
@@ -0,0 +1,9 @@
+﻿namespace URabbitMQ.Shared.Options
+{
+    public class ConnectionStrings
+    {
+        public string? SqlServer { get; set; }
+        public string? RabbitMQ { get; set; }
+        public string? RabbitMQApiUrl { get; set; }
+    }
+}
